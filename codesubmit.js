4c2510f1a810f8291559f757234dc77672a994a9
jrajan20 <jai_20j@hotmail.com>
2018-09-05 15:52:20 +0000

    Adds conditional excercises

---
+++ codesubmit.js
@@ -0,0 +1,7 @@
+var iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+iceCreamFlavours.Push("root beer");
+console.log(iceCreamFlavours);
+console.log(iceCreamFlavours[0]);
+console.log(iceCreamFlavours[5]);
