17c2e78404f200598efc816d0b7736e8e3efd45c
jrajan20 <jai_20j@hotmail.com>
2018-09-06 14:53:56 +0000

    Adds looping excercises

--- codesubmit.js
+++ codesubmit.js
@@ -1,7 +1,3 @@
-var iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
-console.log(iceCreamFlavours);
-
-iceCreamFlavours.Push("root beer");
-console.log(iceCreamFlavours);
-console.log(iceCreamFlavours[0]);
-console.log(iceCreamFlavours[5]);
+console.log(5 * "3");
+console.log(5 + "3");
+console.log(5 % "3");
