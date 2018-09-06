85e1bbeafad752ab10794bccd4c78331c51c8aa5
jrajan20 <jai_20j@hotmail.com>
2018-09-06 18:00:43 +0000

    Saving work done

--- codesubmit.js
+++ codesubmit.js
@@ -1,3 +1,31 @@
-console.log(5 * "3");
-console.log(5 + "3");
-console.log(5 % "3");
+function concat(A1, A2)
+{
+if (A1.length === 0)
+{
+    return A2;
+}
+else if (A2.length === 0)
+{
+    return A1;
+}
+else
+{
+    for (var i = 0; i < A2.length; i++)	
+  {
+    A1.push(A2[i]);
+  }
+}
+  return A1;
+}
+
+}
+
+console.log(concat([ 1, 2, 3 ], [ 4, 5, 6 ]), "=?", [ 1, 2, 3, 4, 5, 6 ]);
+console.log(concat([ 0, 3, 1 ], [ 9, 7, 2 ]), "=?", [ 0, 3, 1, 9, 7, 2 ]);
+console.log(concat([], [ 9, 7, 2 ]), "=?", [ 9, 7, 2 ]);
+console.log(concat([ 5, 10 ], []), "=?", [ 5, 10 ]); 
+  
+  
+
+
+  
